5ddf6821a38fb2f14ab4cc951cd7edd3991aa017
Yuhui Tong <tongyuhui4545@gmail.com>
2022-02-08 12:11:05 +1100

    log in and log out are added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,97 @@
-const registerUser = (req, res) => {
-  res.send("Register Route");
-};
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
 
-const loginUser = (req, res) => {
-  res.send("Login Route");
+//@desc Register a new user
+//route /api/users
+//@access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  //Find if the user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new error("Invalid user data");
+  }
+});
+//@desc Login a user
+//route /api/users/login
+//@access Public
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  //Check user and password match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("invalid credentials");
+  }
+});
+
+//@desc get current user
+//route /api/users/me
+//@access Private
+
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+
+  res.status(200).json(user);
+});
+
+//Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30 days",
+  });
 };
 
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require("colors");
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 8000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+//Connect to database
+connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("hello");
@@ -10,4 +19,7 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
+
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+
+app.use(errorHandler);
